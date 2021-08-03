58e2b13efd00783fb4fe105dc5ff0e755c46ae3c
Danny Jones <daniel@wpwhitesecurity.com>
2021-08-03 16:56:14 +0100

    Add uninstall script

---
+++ uninstall.php
@@ -0,0 +1,9 @@
+<?php
+
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+}
+
+global $wpdb;
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '%anm%'" );
