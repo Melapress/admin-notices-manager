f8d0a3ffe43323c1eaea8fbe7e2e0de02933290b
Danny Jones <daniel@wpwhitesecurity.com>
2021-08-04 11:21:20 +0100

    Add uninstall support for networks

--- uninstall.php
+++ uninstall.php
@@ -5,5 +5,33 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     die;
 }
 
-global $wpdb;
-$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '%anm%'" );
+if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+    $network_id = get_current_network_id();
+    global $wpdb;
+    $wpdb->query(
+        $wpdb->prepare(
+            "
+            DELETE FROM $wpdb->sitemeta
+            WHERE meta_key LIKE %s
+            AND site_id = %d
+            ",
+            [
+                'anm%',
+                $network_id
+            ]
+        )
+    );
+} else {
+    global $wpdb;
+    $wpdb->query(
+        $wpdb->prepare(
+            "
+            DELETE FROM $wpdb->options
+            WHERE option_name LIKE %s
+            ",
+            [
+                'anm%',
+            ]
+        )
+    );
+}
