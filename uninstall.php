43fd1228a2306c62da4ef1dab79327ce5d4f9858
Martin Krcho <martin@wpwhitesecurity.com>
2022-01-26 10:40:04 +0100

    Change formatting in uninstall script.

--- uninstall.php
+++ uninstall.php
@@ -1,34 +1,34 @@
 <?php
 
-// if uninstall.php is not called by WordPress, die
+// If uninstall.php is not called by WordPress, die.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die;
+	die;
 }
 
 if ( function_exists( 'is_multisite' ) && is_multisite() ) {
-    global $wpdb;
-    $wpdb->query(
-        $wpdb->prepare(
-            "
+	global $wpdb;
+	$wpdb->query(
+		$wpdb->prepare(
+			"
             DELETE FROM $wpdb->sitemeta
             WHERE meta_key LIKE %s
             ",
-            [
-                'anm%',
-            ]
-        )
-    );
+			array(
+				'anm%',
+			)
+		)
+	);
 } else {
-    global $wpdb;
-    $wpdb->query(
-        $wpdb->prepare(
-            "
+	global $wpdb;
+	$wpdb->query(
+		$wpdb->prepare(
+			"
             DELETE FROM $wpdb->options
             WHERE option_name LIKE %s
             ",
-            [
-                'anm%',
-            ]
-        )
-    );
+			array(
+				'anm%',
+			)
+		)
+	);
 }
