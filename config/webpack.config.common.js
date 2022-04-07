7a7153bc451e180b8ee94c36c9ba7949d01f97d9
Stoil Dobrev <s.n.dobreff@gmail.com>
2022-04-07 16:40:56 +0300

    Just Test

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
